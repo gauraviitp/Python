326e9011aa2d2941137f7ce875858adadfa3d419
Gaurav Choudhary <cgauraviitp@gmail.com>
2018-11-11 17:42:36 +0000

    Create DepthFirstSearchAdjacencyMatrix.py

---
+++ DepthFirstSearchAdjacencyMatrix.py
@@ -0,0 +1,65 @@
+import sys
+sys.stdin = open('input.txt')
+sys.stdout = open('output.txt', 'w')
+
+f = sys.stdin
+o = sys.stdout
+
+class Node:
+
+    def __init__(self, data):
+        self.data = data
+        self.adjacent = []
+
+class Graph:
+
+    def __init__(self):
+        self.nodes = {}
+
+    def addEdge(self, source, dest):
+        s = self.nodes[source]
+        d = self.nodes[dest]
+        s.adjacent.append(d)
+
+    def visit(self, node):
+        o.write(str(node.data) + ' ')
+
+    def dfs(self, node, s):
+        if node not in s:
+            self.visit(node)
+            s.add(node)
+            for adj in node.adjacent:
+                self.bfs(adj, s)
+
+
+    def depthFirstSearch(self):
+        s = set()
+        for key, node in self.nodes.items():
+            if node not in s:
+                self.bfs(node, s)
+
+
+def main():
+
+    n, e = map(int, f.readline().split())
+
+    g = Graph()
+    nodes = map(int, f.readline().split())
+
+    for _node in nodes:
+        g.nodes[_node] = Node(_node)
+
+    for _e in range(e):
+        s, d = map(int, f.readline().split())
+        g.addEdge(s, d)
+
+    for key, node in g.nodes.items():
+        o.write (str(key) + ': ')
+        for adj in node.adjacent:
+            o.write (str(adj.data) + ' ')
+        o.write('\n')
+
+    g.breadhFirstSearch()
+
+if __name__ == "__main__":
+    main()
